23cfe213f5ece754d2bfd32468b21b524fd88fca
Matteo484126 <matteo.vitobello@studenti.unimi.it>
2025-07-08 10:20:04 +0200

    Update lettoreNFC.ino
    loop sistemato

--- lettoreNFC/lettoreNFC.ino
+++ lettoreNFC/lettoreNFC.ino
@@ -1,31 +1,36 @@
 #include <Wire.h>
 #include <Adafruit_PN532.h>
 #include <MPU6050_tockn.h>
+#include <NewPing.h>
 
 
 // Pin I2C
 #define SDA_PIN 21
 #define SCL_PIN 22
 
-MPU6050 mpu6050(Wire);
-
+// Pin per vari sensori
 #define echo 32
 #define trigger 33
 #define ledSensore 14
-#define ledVerde 2
-#define ledRosso 19
-#define relay 13
+#define ledVerde 26
+#define ledRosso 25
+#define relay 15
 
-// Numero di carte salvate
-const int NUM_CARTE = 4;
-const int UID_LENGTH = 4;
+// SENSORI
+NewPing sonar(trigger, echo, 200);
+MPU6050 mpu6050(Wire);
+Adafruit_PN532 nfc(SDA_PIN, SCL_PIN);
+
+
 
 int erroriConsec = 0;
 
 float accX_offset = 0.0;
 int16_t ax;
 
-float segnale, distanza;
+// Numero di carte salvate
+const int NUM_CARTE = 4;
+const int UID_LENGTH = 4;
 
 uint8_t carteRiconosciute[NUM_CARTE][UID_LENGTH] = {
   {0xFD, 0x40, 0x46, 0x05},
@@ -34,8 +39,6 @@ uint8_t carteRiconosciute[NUM_CARTE][UID_LENGTH] = {
   {0xA5, 0x8B, 0x44, 0x05}
 };
 
-// Inizializza l'oggetto PN532 usando l'I2C
-Adafruit_PN532 nfc(SDA_PIN, SCL_PIN);
 
 void setup(void) {
   Serial.begin(115200);
@@ -74,48 +77,25 @@ void configuraPin(){
   pinMode(ledVerde, OUTPUT);
 }
 
-
-//////////////////////////////////////////////////////////////////////////////// loop
 void loop(void){ 
   uint8_t success;
   uint8_t uid[7];
   uint8_t uidLength;
+  
 
-  if(inMovimento(accX_offset)){
+  Serial.println("controllo se mi devo bloccare");
+  if(inMovimento(accX_offset) && ostacolo()){
     blocca();
   }
 
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 0);
   if (success) {
-    Serial.println("\nCarta NFC rilevata!");
-    Serial.print("UID (lunghezza ");
-    Serial.print(uidLength, DEC);
-    Serial.print("): ");
-    for (uint8_t i = 0; i < uidLength; i++) {
-      Serial.print("0x");
-      Serial.print(uid[i], HEX);
-      Serial.print(" ");
-    }
-    Serial.println();
+    stampaDatiCarta(uid, uidLength);
 
-    delay(500);
+    delay(700);
 
-    bool accettata = false;
-    if (uidLength == UID_LENGTH) {
-      for (uint8_t i = 0; i < NUM_CARTE; i++) {
-        bool match = true;
-        for (uint8_t j = 0; j < UID_LENGTH; j++) {
-          if (uid[j] != carteRiconosciute[i][j]) {
-            match = false;
-            break;
-          }
-        }
-        if (match) {
-          accettata = true;
-          break;
-        }
-      }
-    }
+    bool accettata = trovaCarta(uid, uidLength);
+
     if (accettata) {
       if (ostacolo()) {
         if (inMovimento(accX_offset)) {
@@ -131,8 +111,8 @@ void loop(void){
       cartaRifiutata();
     }
   }
+  delay(50);
 }
-
 
 void cartaRifiutata() {
   Serial.println("Carta Rifiutata!!");
@@ -140,7 +120,7 @@ void cartaRifiutata() {
 
   erroriConsec++;
   if (erroriConsec == 3) {
-    Serial.print("terzo errore riprovare tra 1 min");
+    Serial.print("Terzo errore riprovare tra 1 min");
     delay(60000); // Attende un minuto dopo 3 errori consecutivi
   }
   delay(500);
@@ -149,7 +129,7 @@ void cartaRifiutata() {
 }
 
 void cartaAccettata() {
-  Serial.print("carta accettata");
+  Serial.println("carta accettata");
   erroriConsec = 0;
   digitalWrite(ledVerde, HIGH);
 
@@ -174,7 +154,7 @@ boolean inMovimento(float accX_offset) {
   Serial.print("Movimento: ");
   Serial.println(accX_corrected);
 
-  return accX_corrected > 1 || accX_corrected < -1;
+  return accX_corrected > 0.3 || accX_corrected < -0.3;
 }
 
 void lampeggia(){
@@ -189,16 +169,55 @@ void lampeggia(){
 }
 
 bool ostacolo() {
-  digitalWrite(trigger, HIGH);  
-  delayMicroseconds(10); 
   digitalWrite(trigger, LOW);
-  segnale = pulseIn(echo, HIGH, 30000);
-  distanza = segnale / 58.2;
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+
+  long durata = pulseIn(echo, HIGH, 25000);
+  int distanza = durata * 0.034 / 2;
+
   Serial.print("Distanza: ");
   Serial.println(distanza);
+
   return distanza > 0 && distanza < 15;
 }
 
+
+void stampaDatiCarta(uint8_t uid[],  uint8_t uidLength){
+    Serial.println("\nCarta NFC rilevata!");
+    Serial.print("UID (lunghezza ");
+    Serial.print(uidLength, DEC);
+    Serial.print("): ");
+
+    for (uint8_t i = 0; i < uidLength; i++) {
+      Serial.print("0x");
+      Serial.print(uid[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+}
+
+bool trovaCarta(uint8_t uid[], uint8_t uidLength){
+    if (uidLength == UID_LENGTH) {
+      for (uint8_t i = 0; i < NUM_CARTE; i++) {
+        bool match = true;
+        for (uint8_t j = 0; j < UID_LENGTH; j++) {
+          if (uid[j] != carteRiconosciute[i][j]) {
+            match = false;
+            break;
+          }
+        }
+
+        if (match) {
+          return true;
+          break;
+        }
+      }
+    }
+  return false;
+}
 
 float calibraAccX() {
   float somma = 0;
@@ -209,4 +228,4 @@ float calibraAccX() {
     delay(10);
   }
   return somma / N;
-}
+  }
